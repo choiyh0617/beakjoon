4f2e28a8e4c29227e709f32d4bbe847ace0a15a8
choiyh0617 <6l0mrc00057@gmail.com>
2024-11-26 17:55:22 +0900

    .

---
+++ 2920.py
@@ -0,0 +1,9 @@
+a=list(input().split())
+t1 = ['1', '2', '3', '4', '5', '6', '7', '8']
+t2 = ['8', '7', '6', '5', '4', '3', '2', '1']
+if a == t1 :
+    print('ascending')
+elif a == t2:
+    print('descending')
+else :
+    print('mixed')
